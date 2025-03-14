925fe5e4aaec284e642d92773778156e46ea81db
sandeepa <sanvidurath98@gmail.com>
2025-03-14 23:49:18 +0530

    added dto classes

---
+++ src/main/java/edu/icet/dto/Exam.java
@@ -0,0 +1,11 @@
+package edu.icet.dto;
+
+public class Exam {
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer courseId;
+    private String timeLimit;
+    private Integer passScore;
+    private Integer questionCount;
+}
